7f63c1439cdcfa2910791a51d83d4eb53f1d0900
tigcodu <theonlyjimson@gmail.com>
2025-04-07 07:46:52 +0800

    Update p_puzzle_test.c

--- p_puzzle_test.c
+++ p_puzzle_test.c
@@ -23,22 +23,24 @@ int main(int argc, const char* arg[]) {
 
 
 /*
-    int a = 0;
-    int *p = &a; 
-    
-    while(1) { 
-        
-        printf("Enter value: "); 
-        scanf("%d", p); 
-        if (*p == 0) {
-            break;
-        }
-        p++; 
-    } 
-    --p; 
-    while(*p != 0) { 
-        printf("%d\t", *p); 
-        --p; 
-    } 
-    return 0;
+#include<stdio.h>
+
+int main(){
+    int arr[100];
+	   int *p = arr;
+
+   	scanf("%d",p);
+
+   	while(*p!=0){
+        p++;
+	      	printf("Enter value\n");
+		      scanf("%d",p);
+   	}
+	   --p;
+	   while(p >= arr){
+	      	printf("%d\t",*p);
+	      	--p;
+	}
+ return 0;
+}
 */
